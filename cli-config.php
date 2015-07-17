c9f46dd21de9e6729f9012ae2a2c8db1b6315614
Leszek Manicki <leszek.manicki@wikimedia.de>
2015-07-17 14:08:17 +0200

    Change path to config.ini file, so that running doctrine console in projects depending on FundraisingStore rely on the local config file

--- cli-config.php
+++ cli-config.php
@@ -4,7 +4,7 @@ use Doctrine\DBAL\DriverManager;
 use Doctrine\ORM\Tools\Console\ConsoleRunner;
 use WMDE\Fundraising\Store\Factory;
 
-$config = parse_ini_file( __DIR__ . '/config.ini', true );
+$config = parse_ini_file( 'config.ini', true );
 
 $factory = new Factory( DriverManager::getConnection( $config['database'] ) );
 
