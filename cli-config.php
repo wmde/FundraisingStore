e1ac82294a112e67c892234f6a3442d05faeff26
Leszek Manicki <leszek.manicki@wikimedia.de>
2015-07-17 13:49:41 +0200

    No need to include autoload

--- cli-config.php
+++ cli-config.php
@@ -4,8 +4,6 @@ use Doctrine\DBAL\DriverManager;
 use Doctrine\ORM\Tools\Console\ConsoleRunner;
 use WMDE\Fundraising\Store\Factory;
 
-require_once __DIR__ . '/vendor/autoload.php';
-
 $config = parse_ini_file( __DIR__ . '/config.ini', true );
 
 $factory = new Factory( DriverManager::getConnection( $config['database'] ) );
