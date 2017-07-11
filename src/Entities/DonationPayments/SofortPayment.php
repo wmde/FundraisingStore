dd316e7acca60d4c74912dad7118787f0c1691d0
Gabriel Birke <gabriel.birke@wikimedia.de>
2017-07-11 10:13:45 +0200

    Add confirmation timestamp
    Confirmation timestamp for sofort payments.
    This is for https://phabricator.wikimedia.org/T167882

--- src/Entities/DonationPayments/SofortPayment.php
+++ src/Entities/DonationPayments/SofortPayment.php
@@ -4,6 +4,7 @@ declare( strict_types = 1 );
 
 namespace WMDE\Fundraising\Entities\DonationPayments;
 
+use DateTime;
 use Doctrine\ORM\Mapping as ORM;
 use WMDE\Fundraising\Entities\DonationPayment;
 
@@ -23,6 +24,13 @@ class SofortPayment extends DonationPayment {
 	 */
 	private $bankTransferCode = '';
 
+	/**
+	 * @var DateTime
+	 *
+	 * @ORM\Column(name="confirmed_at", type="datetime", nullable=true)
+	 */
+	private $confirmedAt;
+
 	public function __construct( string $bankTransferCode ) {
 		$this->bankTransferCode = $bankTransferCode;
 	}
@@ -31,4 +39,11 @@ class SofortPayment extends DonationPayment {
 		return $this->bankTransferCode;
 	}
 
+	public function getConfirmedAt(): ?DateTime {
+		return $this->confirmedAt;
+	}
+
+	public function setConfirmedAt( DateTime $confirmedAt ) {
+		$this->confirmedAt = $confirmedAt;
+	}
 }
