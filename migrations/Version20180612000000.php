4b26975eceec41e7e1acb137c6ce54fef77da7f7
Tim Eulitz <tim.eulitz@wikimedia.de>
2018-06-21 11:13:14 +0200

    Fix potential DB pagination issue + formatting fix
    T196333

--- migrations/Version20180612000000.php
+++ migrations/Version20180612000000.php
@@ -33,14 +33,14 @@ class Version20180612000000 extends AbstractMigration {
 		);
 		$this->skipIf( $schema->hasTable( 'address_change' ), 'AddressChange table already exists' );
 
-		$this->addSql('CREATE TABLE address_change (id INT AUTO_INCREMENT NOT NULL, address_id INT DEFAULT NULL, current_identifier VARCHAR(36) DEFAULT NULL, previous_identifier VARCHAR(36) DEFAULT NULL, UNIQUE INDEX UNIQ_7B0E7B9FA8954A18 (current_identifier), UNIQUE INDEX UNIQ_7B0E7B9F2EC1D3 (previous_identifier), INDEX IDX_7B0E7B9FF5B7AF75 (address_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
-		$this->addSql('ALTER TABLE address_change ADD CONSTRAINT FK_7B0E7B9FF5B7AF75 FOREIGN KEY (address_id) REFERENCES address (id)');
-		$this->addSql('ALTER TABLE spenden ADD address_change_id INT DEFAULT NULL');
-		$this->addSql('ALTER TABLE spenden ADD CONSTRAINT FK_3CBBD045BB7DB7BC FOREIGN KEY (address_change_id) REFERENCES address_change (id)');
-		$this->addSql('CREATE UNIQUE INDEX UNIQ_3CBBD045BB7DB7BC ON spenden (address_change_id)');
-		$this->addSql('ALTER TABLE request ADD address_change_id INT DEFAULT NULL');
-		$this->addSql('ALTER TABLE request ADD CONSTRAINT FK_3B978F9FBB7DB7BC FOREIGN KEY (address_change_id) REFERENCES address_change (id)');
-		$this->addSql('CREATE UNIQUE INDEX UNIQ_3B978F9FBB7DB7BC ON request (address_change_id)');
+		$this->addSql( 'CREATE TABLE address_change (id INT AUTO_INCREMENT NOT NULL, address_id INT DEFAULT NULL, current_identifier VARCHAR(36) DEFAULT NULL, previous_identifier VARCHAR(36) DEFAULT NULL, UNIQUE INDEX UNIQ_7B0E7B9FA8954A18 (current_identifier), UNIQUE INDEX UNIQ_7B0E7B9F2EC1D3 (previous_identifier), INDEX IDX_7B0E7B9FF5B7AF75 (address_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB' );
+		$this->addSql( 'ALTER TABLE address_change ADD CONSTRAINT FK_7B0E7B9FF5B7AF75 FOREIGN KEY (address_id) REFERENCES address (id)' );
+		$this->addSql( 'ALTER TABLE spenden ADD address_change_id INT DEFAULT NULL' );
+		$this->addSql( 'ALTER TABLE spenden ADD CONSTRAINT FK_3CBBD045BB7DB7BC FOREIGN KEY (address_change_id) REFERENCES address_change (id)' );
+		$this->addSql( 'CREATE UNIQUE INDEX UNIQ_3CBBD045BB7DB7BC ON spenden (address_change_id)' );
+		$this->addSql( 'ALTER TABLE request ADD address_change_id INT DEFAULT NULL' );
+		$this->addSql( 'ALTER TABLE request ADD CONSTRAINT FK_3B978F9FBB7DB7BC FOREIGN KEY (address_change_id) REFERENCES address_change (id)' );
+		$this->addSql( 'CREATE UNIQUE INDEX UNIQ_3B978F9FBB7DB7BC ON request (address_change_id)' );
 	}
 
 	/**
@@ -48,8 +48,7 @@ class Version20180612000000 extends AbstractMigration {
 	 *
 	 * @throws \Doctrine\DBAL\DBALException
 	 */
-	public function postUp(Schema $schema)
-	{
+	public function postUp( Schema $schema ) {
 		$this->updateAddressChangeForeignKeys( self::DB_TABLE_DONATIONS );
 		$this->updateAddressChangeForeignKeys( self::DB_TABLE_MEMBERSHIP_APPLICATIONS );
 	}
@@ -61,7 +60,7 @@ class Version20180612000000 extends AbstractMigration {
 	 */
 	private function updateAddressChangeForeignKeys( $table ): void {
 		$entityCount = $this->getAddressChangeNullColumnCount( $table );
-		$pageCount = (int) ($entityCount / self::DB_QUERY_PAGE_SIZE);
+		$pageCount = (int) ceil( $entityCount / self::DB_QUERY_PAGE_SIZE );
 		$currentAddressChangeId = $this->getHighestAddressChangeId();
 		$this->connection->beginTransaction();
 		$query = '';
@@ -74,23 +73,25 @@ class Version20180612000000 extends AbstractMigration {
 				$stmt->closeCursor();
 				$this->connection->commit();
 
-				if ($entityCount - 1 !== $currentEntity) {
+				if ( $entityCount - 1 !== $currentEntity ) {
 					$this->connection->beginTransaction();
 				}
-				echo $currentEntity . " AddressChange entitites committed\n";
+				$this->write( $currentEntity . " / $entityCount\n" );
 				$query = '';
 			}
 		}
 
 		for ( $page = 0; $page < $pageCount; $page++ ) {
 			$entities = $this->getPageForTable( $table );
-			echo 'Page: ' . $page . ' / ' . $pageCount . "\n";
+			$this->write( 'Page: ' . $page . ' / ' . $pageCount . "\n" );
 			foreach ( $entities as $entity ) {
 				$currentAddressChangeId++;
-				echo 'UPDATE ' . $table . ' SET address_change_id = '. $currentAddressChangeId . ' WHERE id = ' . (int) $entity['id'] . "\n";
+				$this->write(
+					'UPDATE ' . $table . ' SET address_change_id = ' . $currentAddressChangeId . ' WHERE id = ' . (int)$entity['id'] . "\n"
+				);
 				$this->connection->executeUpdate(
 					'UPDATE ' . $table . ' SET address_change_id = ? WHERE id = ?',
-					[ $currentAddressChangeId, (int) $entity['id'] ]
+					[ $currentAddressChangeId, (int)$entity['id'] ]
 				);
 			}
 		}
@@ -110,19 +111,19 @@ class Version20180612000000 extends AbstractMigration {
 		$queryBuilder->select( 'id' )
 			->from( $table )
 			->where( 'address_change_id is NULL' )
-			->orderBy('id', 'ASC')
-			->setMaxResults(  self::DB_QUERY_PAGE_SIZE );
+			->orderBy( 'id', 'ASC' )
+			->setMaxResults( self::DB_QUERY_PAGE_SIZE );
 		return $queryBuilder->execute();
 	}
 
 	private function getHighestAddressChangeId(): int {
 		$dbResult = $this->connection->createQueryBuilder()
-			->select('id')
-			->from('address_change')
-			->orderBy('id', 'DESC')
-			->setMaxResults(1)
-			->execute()->fetchColumn(0);
-		return (int) $dbResult;
+			->select( 'id' )
+			->from( 'address_change' )
+			->orderBy( 'id', 'DESC' )
+			->setMaxResults( 1 )
+			->execute()->fetchColumn( 0 );
+		return (int)$dbResult;
 	}
 
 	/**
@@ -131,16 +132,18 @@ class Version20180612000000 extends AbstractMigration {
 	 * @throws \Doctrine\DBAL\DBALException
 	 * @throws \Doctrine\DBAL\Migrations\AbortMigrationException
 	 */
-	public function down(Schema $schema): void
-	{
-		$this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+	public function down( Schema $schema ): void {
+		$this->abortIf(
+			$this->connection->getDatabasePlatform()->getName() !== 'mysql',
+			'Migration can only be executed safely on \'mysql\'.'
+		);
 
-		$this->addSql('ALTER TABLE spenden DROP FOREIGN KEY FK_3CBBD045BB7DB7BC');
-		$this->addSql('ALTER TABLE request DROP FOREIGN KEY FK_3B978F9FBB7DB7BC');
-		$this->addSql('DROP TABLE address_change');
-		$this->addSql('DROP INDEX UNIQ_3B978F9FBB7DB7BC ON request');
-		$this->addSql('ALTER TABLE request DROP address_change_id');
-		$this->addSql('DROP INDEX UNIQ_3CBBD045BB7DB7BC ON spenden');
-		$this->addSql('ALTER TABLE spenden DROP address_change_id');
+		$this->addSql( 'ALTER TABLE spenden DROP FOREIGN KEY FK_3CBBD045BB7DB7BC' );
+		$this->addSql( 'ALTER TABLE request DROP FOREIGN KEY FK_3B978F9FBB7DB7BC' );
+		$this->addSql( 'DROP TABLE address_change' );
+		$this->addSql( 'DROP INDEX UNIQ_3B978F9FBB7DB7BC ON request' );
+		$this->addSql( 'ALTER TABLE request DROP address_change_id' );
+		$this->addSql( 'DROP INDEX UNIQ_3CBBD045BB7DB7BC ON spenden' );
+		$this->addSql( 'ALTER TABLE spenden DROP address_change_id' );
 	}
 }
