7a06e32c72e04152a720979bc1ec054927e6a028
tzhelyazkova <tonina.zhelyazkova@wikimedia.de>
2019-05-24 11:56:57 +0200

    Add migration script

--- migrations/Version20190524000000.php
+++ migrations/Version20190524000000.php
@@ -6,23 +6,26 @@ use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
 /**
- * Auto-generated Migration: Please modify to your needs!
+ * This class alters the Address table
+ * It adds the donation_receipt column so that Address is directly storing the opt in preference
+ *
+ * This script should only be executed in maintenance mode, as the ALTER table queries may take a while to process
+ *
+ * @package DoctrineMigrations
  */
-final class Version20190524094504 extends AbstractMigration
+final class Version20190524000000 extends AbstractMigration
 {
-    public function up(Schema $schema) : void
-    {
-        // this up() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+	public function up(Schema $schema) : void
+	{
+		$this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE address ADD donation_receipt TINYINT(1) NOT NULL');
-    }
+		$this->addSql('ALTER TABLE address ADD donation_receipt TINYINT(1) NOT NULL');
+	}
 
-    public function down(Schema $schema) : void
-    {
-        // this down() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+	public function down(Schema $schema) : void
+	{
+		$this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE address DROP donation_receipt');
-    }
+		$this->addSql('ALTER TABLE address DROP donation_receipt');
+	}
 }
