f56b1929d18c7e808fa22c9b88bf1b72ffb6037a
Tonina <tonina.zhelyazkova@wikimedia.de>
2019-05-27 14:27:02 +0200

    Update migrations/Version20190524000000.php
    Co-Authored-By: Gabriel Birke <gabriel.birke@gmail.com>

--- migrations/Version20190524000000.php
+++ migrations/Version20190524000000.php
@@ -26,6 +26,6 @@ final class Version20190524000000 extends AbstractMigration
 	{
 		$this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-		$this->addSql('ALTER TABLE address DROP donation_receipt');
+		$this->addSql('ALTER TABLE address_change DROP donation_receipt');
 	}
 }
